e8cf9b134cf79d70365f863af97568d63763e3f8
abdulrehman1004 <rehman.abdul008844@gmail.com>
2024-10-23 23:21:36 +0500

    add

---
+++ 08-Langchain/5_rag/4_vecor_store/vecor_store.py
@@ -0,0 +1,26 @@
+# Install Some Packages ()
+
+# Import Packages
+
+# Load the PDF document using PyPDFLoader
+
+# Split the document into chunks using Splitter
+
+# MetaData Processing:
+
+# Initialize HuggingFace Embedding Model
+
+# Initialize Google Embedding Model
+
+# =========================== Create and Add Embedding to the Qdrant Vector Store  ===========================
+
+# ========== Create Embedding using HuggingFace Model ==========
+
+# ========== Create Embedding using Google Gemini Model ==========
+
+
+# =========================== Create and Add Embedding to the Pinecone Vector Store  ===========================
+
+# ========== Create Embedding using HuggingFace Model ==========
+
+# ========== Create Embedding using Google Gemini Model ==========
